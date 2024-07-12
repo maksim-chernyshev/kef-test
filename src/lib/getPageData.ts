21922de7317d8e6c42d8ff42c25a112ad0a71ab5
maksim_chernyshev <maksim.tch@gmail.com>
2024-07-12 14:25:03 +0400

    Fix request error

--- src/lib/getPageData.ts
+++ src/lib/getPageData.ts
@@ -6,11 +6,20 @@ interface IPageData {
     comments: IComment[];
 }
 
-export const getPageData = async (pageNumber: number): Promise<IPageData> => {
-    const pageInfo = await getCommentsRequest(pageNumber);
+export const getPageData = async (pageNumber: number, retryCount = 5): Promise<IPageData> => {
+    try {
+        const pageInfo = await getCommentsRequest(pageNumber);
 
-    return {
-        pagination: pageInfo.pagination,
-        comments: pageInfo.data,
-    };
+        return {
+            pagination: pageInfo.pagination,
+            comments: pageInfo.data,
+        };
+    } catch (error) {
+        if (retryCount > 0) {
+            console.error(`Ошибка загрузки данных. Попытка №${6 - retryCount}`);
+            return getPageData(pageNumber, retryCount - 1);
+        } else {
+            throw new Error("Не удалось загрузить данные страницы");
+        }
+    }
 };
