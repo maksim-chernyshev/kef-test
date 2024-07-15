ff2f6d8ee1a75f55ce173becdecc7a66a54e45d7
maksim_chernyshev <maksim.tch@gmail.com>
2024-07-15 11:43:17 +0400

    Fix styles

--- src/components/Comments/styled.ts
+++ src/components/Comments/styled.ts
@@ -19,22 +19,6 @@ export const CommentsStyled = styled.ul`
             padding-left: 20px;
         }
     }
-
-    + button {
-        width: max-content;
-        margin: 0 auto;
-        padding: 8px 31px;
-        border: none;
-        font-size: 16px;
-        line-height: 22px;
-        color: #ffffff;
-        background-color: #313439;
-        cursor: pointer;
-
-        &:hover {
-            background-color: #767676;
-        }
-    }
 `;
 
 export const CommentsErrorStyled = styled.div`
